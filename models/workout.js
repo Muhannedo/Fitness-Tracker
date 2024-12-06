fde53b89efa43013496d9673c427f82afda68681
Muhannad <muhanned9980@gmail.com>
2024-12-06 22:34:49 +0300

    adding WorkoutSchema

---
+++ models/workout.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const workoutSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: false,
+  },
+  workoutType: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  exercise: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "exercise",
+    },
+  ],
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
